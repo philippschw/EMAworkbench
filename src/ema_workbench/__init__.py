0208145bbbe066b6201393b7abc61ee94c03e691
philippschw <philipp.schw@gmail.com>
2016-12-07 10:59:54 +0100

    Update __init__.py

--- src/ema_workbench/__init__.py
+++ src/ema_workbench/__init__.py
@@ -6,6 +6,6 @@ from .em_framework import (Model, RealParameter, CategoricalParameter,
                            TimeSeriesOutcome, Constant)
 
 from . import util
-from .util import save_results, load_results, ema_logging
+from .util import save_results, load_results, ema_logging, save_EMA_results, load_EMA_results
 
 __version__ = '0.7.0.dev1'
